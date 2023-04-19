004fb80602fefbecc122d31dc8d95a4a1ade118e
ivanferreira-java <ivan.ir.cjava@gmail.com>
2023-04-19 12:42:26 -0300

    ADD o campo idade a classe pessoa

--- projetojava1/src/cursojava/Pessoa.java
+++ projetojava1/src/cursojava/Pessoa.java
@@ -4,6 +4,7 @@ public class Pessoa {
 	
 	private String nome;
 	private String sobrenome;
+	private int idade;
 	
 	
 	public String getNome() {
@@ -18,7 +19,11 @@ public class Pessoa {
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
-	
-	
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 
 }
